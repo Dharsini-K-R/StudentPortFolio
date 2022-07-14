f86e6091b855dc4830662f7b1ff19a1cc881192b
unknown <dharsinikr2000@gmail.com>
2022-07-14 20:28:38 +0530

    StudentPortFolio

---
+++ StudentPortFolio/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentPortFolio.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required(ErrorMessage = "Please Enter Name.")]
+        public string StudentName { get; set; }
+        [Required(AllowEmptyStrings = true)]
+        public int Age { get; set; }
+        [Column(TypeName = "nvarchar(30)")]
+        public string? Location { get; set; }
+        public int Rank { get; set; }
+
+    }
+}
---
+++ StudentPortFolio/Models/StudentDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortFolio.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options) : base(options)
+        {
+
+        }
+
+        //Create Table here
+        public DbSet<Student> Students { get; set; }
+    }
+}
